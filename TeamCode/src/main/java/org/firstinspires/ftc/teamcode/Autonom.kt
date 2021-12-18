abdd190b5cab2e28a4dc4a742bf1c83a0b08d8f7
Boogeybots <boogeybots@gmail.com>
2021-12-18 14:30:35 +0200

    Adaugare detectare

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonom.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonom.kt
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.Servo
+import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap
 
 @Autonomous(name = "Autonom aruncat", group = "Sensor")
@@ -18,17 +19,22 @@ class Autonom : LinearOpMode(){
        servoImpins?.position = 0.5
 
         waitForStart()
-        Thread.sleep(1000)
+        wait(1000.0)
         dcAruncat?.power = 0.65
         for(i in 1..3) {
 
-           Thread.sleep(3000)
+           wait(3000.0)
            servoImpins?.position = 0.0
-           Thread.sleep(1000)
+           wait(1000.0)
            servoImpins?.position = 0.5
        }
        dcAruncat?.power = 0.0
     }
 
+    fun wait(mil: Double){
+        val time = ElapsedTime()
+        while(opModeIsActive() && time.milliseconds() < mil){
+        }
+    }
 
 }
